399a5912cd8546989644c3875f0e8a5ad00a2a2b
RN848 <you@example.com>
2024-10-29 21:02:24 +0300

    nearly finsh frame 2

--- src/pages/SportsReserve.js
+++ src/pages/SportsReserve.js
@@ -14,18 +14,71 @@ import { Radio } from "../components/Inputs";
 import { Uplode } from "../components/Inputs";
 import { Link } from "../components/Inputs";
 
-import reserve from "../images/home/reserve.jpg";
-import clups from "../images/home/clups.jpg";
+import Basketball from "../images/sports/sport-1.jpg";
+import Football from "../images/sports/sport-2.jpg";
+import tennis from "../images/sports/sport-3.jpg";
+import Volleyball from "../images/sports/sport-4.jpg";
+import Squash from "../images/sports/sport-5.jpg";
+import Badminton from "../images/sports/sport-6.jpg";
 
 const SportsReserve = () => {
-  const [inputs, setInputs] = useState({
-    title: "",
-    subtitle: "",
-    desc: "",
-    type: "activity",
-    appearance: "all",
-    joinLink: "dfdffdf",
-    image: "No file chosen",
+  const [sport, setsport] = useState({
+    filter: "",
+  });
+
+  const sportsList = [
+    { name: "Basketball", image: Basketball },
+    { name: "Football", image: Football },
+    { name: "tennis", image: tennis },
+    { name: "Volleyball", image: Volleyball },
+    { name: "Squash", image: Squash },
+    { name: "Badminton", image: Badminton },
+  ];
+
+  const sportsMap = sportsList.map((s) => {
+    return (
+      <div>
+        <a href="">
+          <Image src={s.image}></Image>
+        </a>
+        <span>{s.name}</span>
+      </div>
+    );
+  });
+
+  const reservesList = [
+    { sport: "Football", feild: "Feild 1", day: "Sunday", time: "1:00" },
+    { sport: "Basketball", feild: "Feild 2", day: "monday", time: "1:00" },
+    { sport: "tennis", feild: "Feild 1", day: "Sunday", time: "1:00" },
+    { sport: "Football", feild: "Feild 3", day: "friday", time: "1:00" },
+    { sport: "Basketball", feild: "Feild 4", day: "Sunday", time: "1:00" },
+    { sport: "Football", feild: "Feild 4", day: "monday", time: "1:00" },
+    { sport: "Football", feild: "Feild 1", day: "Sunday", time: "1:00" },
+    { sport: "Volleyball", feild: "Feild 6", day: "friday", time: "1:00" },
+    { sport: "Football", feild: "Feild 2", day: "monday", time: "1:00" },
+    { sport: "tennis", feild: "Feild 2", day: "Sunday", time: "1:00" },
+    { sport: "Badminton", feild: "Feild 1", day: "friday", time: "1:00" },
+    { sport: "Football", feild: "Feild 3", day: "Sunday", time: "1:00" },
+    { sport: "Squash", feild: "Feild 1", day: "Sunday", time: "1:00" },
+    { sport: "Squash", feild: "Feild 3", day: "monday", time: "1:00" },
+    { sport: "Football", feild: "Feild 1", day: "Sunday", time: "1:00" },
+  ];
+
+  const reserveMap = reservesList.map((box) => {
+    return (
+      <Col lg={4} md={4} sm={6} xs={12} className="sport-col">
+        <a href="">
+          <div>
+            <h3>{box.sport}</h3>
+            <div className="deteils">
+              <span>{box.feild}</span>
+              <span>{box.day}</span>
+              <span>{box.time}</span>
+            </div>
+          </div>
+        </a>
+      </Col>
+    );
   });
 
   return (
@@ -37,23 +90,13 @@ const SportsReserve = () => {
           style={{ marginBottom: "1.5em" }}
         >
           <Col lg={8} md={8} sm={12}>
-            <div className="wid-colum"></div>
+            <div className={"wid-colum sports"}>{sportsMap}</div>
           </Col>
           <Col>
             <div className="wid-colum"></div>
           </Col>
         </Row>
-        <Row className={"g-4 wid-row "}>
-          <Col lg={12}>
-            <div className="wid-colum">
-              <Row className={"g-4 wid-row "} md={3} sm={2} xs={1}>
-                <Col>
-                  <div className={"wid-colum reserv-card"}></div>
-                </Col>
-              </Row>
-            </div>
-          </Col>
-        </Row>
+        <Row className={"g-4 wid-row sports-box"}>{reserveMap}</Row>
       </div>
     </Body>
   );
