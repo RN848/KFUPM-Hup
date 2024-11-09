f2dd76bfb38a9fd09fe535c33e8e5e863ca9660a
Omar <145347533+OmarAlkhulaif@users.noreply.github.com>
2024-11-10 02:30:17 +0300

    Modify frame 25 again

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import NoPage from "./pages/NoPage";
+import { Suspense } from "react";
 
 import CreateActivity from "./pages/CreateActivity";
 import ManageProfile from "./pages/ManageProfile";
@@ -36,76 +37,79 @@ function App() {
   return (
     <BrowserRouter>
       <Suspense fallback={<LoadingPage />}>
-      <Routes>
-        <Route>
-          <Route index element={<Home />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
+        <Routes>
+          <Route>
+            <Route index element={<Home />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<NoPage />} />
 
-          <Route path="Create-Activity-news" element={<CreateActivity />} />
-          {/*{frame 13}*/}
+            <Route path="Create-Activity-news" element={<CreateActivity />} />
+            {/*{frame 13}*/}
 
-          <Route path="Manage-Profile" element={<ManageProfile />} />
-          {/*{frame 14}*/}
+            <Route path="Manage-Profile" element={<ManageProfile />} />
+            {/*{frame 14}*/}
 
-          <Route path="Your-Profile" element={<YourProfile />} />
-          {/*{frame 20}*/}
+            <Route path="Your-Profile" element={<YourProfile />} />
+            {/*{frame 20}*/}
 
-          <Route path="Log-In" element={<LogIn />} />
-          {/*{frame 18}*/}
+            <Route path="Log-In" element={<LogIn />} />
+            {/*{frame 18}*/}
 
-          <Route path="Sign-Up" element={<SignUp />} />
-          {/*{frame 19}*/}
+            <Route path="Sign-Up" element={<SignUp />} />
+            {/*{frame 19}*/}
 
-          <Route path="New-Clup" element={<NewClup />} />
-          {/*{frame 22}*/}
+            <Route path="New-Clup" element={<NewClup />} />
+            {/*{frame 22}*/}
 
-          <Route path="Sports-reservation" element={<SportsReserve />} />
-          {/*{frame 22}*/}
+            <Route path="Sports-reservation" element={<SportsReserve />} />
+            {/*{frame 22}*/}
 
-          <Route path="Latest-News" element={<LatestNews />} />
-          {/*{frame 8}*/}
+            <Route path="Latest-News" element={<LatestNews />} />
+            {/*{frame 8}*/}
 
-          <Route path="edit-Activity" element={<EditActivity />} />
-          {/*{frame 15}*/}
+            <Route path="edit-Activity" element={<EditActivity />} />
+            {/*{frame 15}*/}
 
-          <Route path="new-reservation" element={<NewReservation />} />
-          {/*{frame 3}*/}
+            <Route path="new-reservation" element={<NewReservation />} />
+            {/*{frame 3}*/}
 
-          <Route path="reservaion-success" element={<ReservationSuccess />} />
-          {/*{frame 4}*/}
+            <Route path="reservaion-success" element={<ReservationSuccess />} />
+            {/*{frame 4}*/}
 
-          <Route
-            path="latest-newsAndActivity"
-            element={<LatestNewsAndActivites />}
-          />
-          {/*{frame 7}*/}
+            <Route
+              path="latest-newsAndActivity"
+              element={<LatestNewsAndActivites />}
+            />
+            {/*{frame 7}*/}
 
-          <Route path="activity-view" element={<ActivityView />} />
-          {/*{frame 7}*/}
+            <Route path="activity-view" element={<ActivityView />} />
+            {/*{frame 7}*/}
 
-          <Route path="/reservation-details" element={<ReservationDetails />} />
-          {/*{frame 5&6}*/}
+            <Route
+              path="/reservation-details"
+              element={<ReservationDetails />}
+            />
+            {/*{frame 5&6}*/}
 
-          <Route path="/club-members" element={<ClubMembers />} />
-          {/*{frame 16}*/}
+            <Route path="/club-members" element={<ClubMembers />} />
+            {/*{frame 16}*/}
 
-          <Route path="/member-profile" element={<MemberProfile />} />
-          {/*{frame 17}*/}
+            <Route path="/member-profile" element={<MemberProfile />} />
+            {/*{frame 17}*/}
 
-          <Route path="/clubs" element={<Clubs />} />
-          {/*{frame 21}*/}
+            <Route path="/clubs" element={<Clubs />} />
+            {/*{frame 21}*/}
 
-          <Route path="/edit-club" element={<EditClub />} />
-          {/*{frame 23}*/}
+            <Route path="/edit-club" element={<EditClub />} />
+            {/*{frame 23}*/}
 
-          <Route path="/error" element={<ErrorPage />} />
-          {/*{frame 24}*/}
+            <Route path="/error" element={<ErrorPage />} />
+            {/*{frame 24}*/}
 
-          <Route path="/loading" element={<LoadingPage />} />
-          {/*{frame 25}*/}
-        </Route>
-      </Routes>
+            <Route path="/loading" element={<LoadingPage />} />
+            {/*{frame 25}*/}
+          </Route>
+        </Routes>
       </Suspense>
     </BrowserRouter>
   );
